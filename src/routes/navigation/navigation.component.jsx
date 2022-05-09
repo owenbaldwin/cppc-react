d01f9c0674f1bc0439244d26aba4710217647ce1
Owen Baldwin <owenabaldwin@gmail.com>
2022-05-09 16:15:36 +0100

    basic navbar done and hamburger working

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,13 +4,20 @@ import './navigation.styles.scss';
 const Navigation = () => {
   return (
     <div className='navigation-container'>
-      <div className='navbar'>
-        <p>About</p>
-        <p>What We Do</p>
-        <p>How we do it</p>
-        <p>Case studies</p>
-        <p>Contact</p>
-        <p>Blog</p>
+      <div className='navigation-inner'>
+        <div className='nav-blur'></div>
+        <div className='navbar'>
+          <input type="checkbox" class="menu-btn" id="menu-btn" />
+          <label for="menu-btn" class="menu-icon"><span class="nav-icon"></span></label>
+          <ul class="tabs">
+            <li><p>About</p></li>
+            <li><p>What We Do</p></li>
+            <li><p>How we do it</p></li>
+            <li><p>Case studies</p></li>
+            <li><p>Contact</p></li>
+            <li><p>Blog</p></li>
+          </ul>
+        </div>
       </div>
       <Outlet />
     </div>
