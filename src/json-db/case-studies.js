8d83dc6d719626ce6b174ae7b124db5c7ec99734
Owen Baldwin <owenabaldwin@gmail.com>
2022-05-10 16:11:53 +0100

    case studies and references done

---
+++ src/json-db/case-studies.js
@@ -0,0 +1,46 @@
+const CASE_STUDIES = [
+  {
+    id: 1,
+    imageUrl: "https://i.ibb.co/YXfVGr2/1881d2-45799730db1f4d9c9787045a437d0894-mv2.webp",
+    text: "Fortnum & Mason is as iconic in luxury food as it's possible to be. Here at CPPC we were charged with bringing their luxury mince pie collection to life with food grade carton gift boxes using Pantone litho printing and intricate high definition foiling. Along the way among the many challenges we met the biggest one was preventing all that butter from migrating through the board while avoiding any polymer based laminates that would impact the recyclability of the boxes.",
+    link: "https://www.fortnumandmason.com/",
+    title: "fortnumandmason"
+  },
+  {
+    id: 2,
+    imageUrl: "https://i.ibb.co/J2qKtnn/1881d2-fb348a1598a145b8a17fe217a9bfdb98-mv2.webp",
+    text: "Established twenty years ago, Tilly Sveaas founded her eponymous jewellery brand whilst on a prolonged gap year in Bali. Tilly wanted CPPC to create a revised set of gift packaging that would enhance and uphold the core values of her stunning jewellery and brand but with a keen eye on the environment. Using a pallet of deep dyed paper throughout the range and marrying that with the textile elements of the gift sets the result is minimal and neutral to enhance the unboxing experience.",
+    link: "https://tillysveaas.co.uk/",
+    title: "tillysveaas.co.uk"
+  },
+  {
+    id: 3,
+    imageUrl: "https://i.ibb.co/DVFfP1t/1881d2-3bfdc17f72454864b1c3457448f9c20a-mv2.webp",
+    text: "Akt is a London based premium skincare label that creates intelligent deodorant formulations BORN TO PERFORM however active your lifestyle might be. CPPC has been integral in not only the development of the core packaging range, from the bold use of primary deep dyed colorplan paper stocks to the supply of their 100% aluminium tubes, their bespoke ecommerce packaging, their gifting range and also the development of new product lines as we speak.",
+    link: "https://www.aktlondon.com/",
+    title: "aktlondon.com"
+  },
+  {
+    id: 4,
+    imageUrl: "https://i.ibb.co/vq2xr65/owllark-black-min.webp",
+    text: "At Owl and Lark it's their mission to empower people to sleep better night after night after night after night. So the challenge was to deliver their extraordinary mattresses in a sustainable and creative way that suited the usability of the brand. CPPC helped to develop a one size fits all roll top carry bag that once the mattress was delivered could have an onward life in your home as the ultimate utility bag.",
+    link: "https://owllark.com/",
+    title: "owllark.com"
+  },
+  {
+    id: 5,
+    imageUrl: "https://i.ibb.co/sKFtr3f/NEAT-Logo-RGB-500x.webp",
+    text: "Simple, Real, Bold, Positive are all the touchstones of Neat. Creating a sustainable solution in homewares and personal care Neat have developed an infinitely refillable range of products that do away with single use plastic completely. Our planet is in crisis and is changing fast, but Neat felt this was not reflected in the choices available to the consumer and here at CPPC we have been thrilled to be engaged on this positive move towards a better future.",
+    link: "https://www.neatclean.london/",
+    title: "neatclean.london"
+  },
+  {
+    id: 6,
+    imageUrl: "https://i.ibb.co/P1dxczW/2x-E-Namsu-Logo-Hi-Res-01-180x-webp.webp",
+    text: "Namsu take their lead from nature. A personal care brand using natural plant-derived ingredients that cleanse and rejuvenate both body and mind. With a packaging suite that is designed and made to last. Insert the refill inside the outer case, affix the pump top. Reuse, recycle and reduce waste. ",
+    link: "https://namsu.me/",
+    title: "namsu.me"
+  },
+]
+
+export default CASE_STUDIES;
---
+++ src/json-db/references.js
@@ -0,0 +1,19 @@
+const REFERENCES = [
+  {
+    id: 1,
+    name: "TILLY SVEAAS",
+    text: "Lex at CPPC is great to work with. He understood the brand aesthetic really well and helped us produce packaging that has elevated our product and the customer experience. He is super easy to deal with and communicates the stages of production clearly. He is a pleasure to work with.",
+  },
+  {
+    id: 2,
+    name: "NEAT.",
+    text: "If only I had connected with Alex and the team at CPPC earlier... it would have saved me not only money but a whole lot of trouble. CPPC are a rare find with an incredible network and knowledge of the packaging and product development industry and can basically solve any packaging problem. Not only that but they do it with agility, great results and are a pleasure to work with.",
+  },
+  {
+    id: 3,
+    name: "OWL & LARK",
+    text: "As a lean product development business, working with Alex and CPPC has been one of the best decisions we could have taken. His experience with everything from eco materials down to the detail of manufacturing processes has opened us to new product possibilities and elevated our existing brands far beyond what we thought was possible or affordable.",
+  },
+]
+
+export default REFERENCES;
