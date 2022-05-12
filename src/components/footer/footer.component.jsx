b951e878e2f94a6fb8472f7f44f13f90fdf49303
Owen Baldwin <owenabaldwin@gmail.com>
2022-05-12 10:20:00 +0100

    footer done

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,12 @@
+import './footer.styles.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <img src="https://i.ibb.co/0GFC4sC/Approved-Partner-CMYK-Grey-Solid.webp" alt="stamp" className='footer-stamp'/>
+      <img src="https://i.ibb.co/9Y66sNq/LOGO-LANDSCAPE-TRANSPARENT.webp" alt="logo" className='footer-logo'/>
+    </div>
+  );
+};
+
+export default Footer;
