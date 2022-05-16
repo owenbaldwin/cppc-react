08294d06eca650f43678eaa6762549bef567ca33
Owen Baldwin <owenabaldwin@gmail.com>
2022-05-16 17:50:33 +0100

    blog post five done

---
+++ src/json-db/blog-post-5.js
@@ -0,0 +1,46 @@
+const BLOG_POST_FIVE = [
+  {
+    title: "Roam",
+    logo: "https://i.ibb.co/W2Bh5TV/218864833-104189261952256-8057249394250488165-n.webp",
+    photos: [
+      {
+        id: 1,
+        imageUrl: "https://i.ibb.co/kS3jVkD/Roam-6.webp"
+      },
+      {
+        id: 2,
+        imageUrl: "https://i.ibb.co/grRWtTP/roam-1.webp"
+      },{
+        id: 3,
+        imageUrl: "https://i.ibb.co/qR7bVML/Roam-3.webp"
+      },{
+        id: 4,
+        imageUrl: "https://i.ibb.co/m5qFVw3/Roam-2.webp"
+      },
+    ],
+    text: [
+      {
+        id: 5,
+        paragraph: "This month CPPC brings you the wonderful world of Roam, who exists to make the world a more sex positive place and to break down stigmas surrounding sex with 100% natural products for all.",
+      },
+      {
+        id: 6,
+        paragraph: "Roam is 100% natural and only ever uses ingredients found in nature. All their products are vegan, they’re proudly plant-based and cruelty free.",
+      },
+      {
+        id: 7,
+        paragraph: "Roam is also sustainable with all their packaging being recyclable and made from 100% recycled materials.",
+      },
+      {
+        id: 8,
+        paragraph: "By only using Fair Rubber in our condoms, Roam pays a premium to ensure rubber farmers can build a more sustainable future for their families.",
+      },
+      {
+        id: 9,
+        paragraph: " to get involved and see the creative packaging solutions we have brought to the table (and the bedroom).",
+      },
+    ]
+  }
+]
+
+export default BLOG_POST_FIVE;
