ced4cca5cc910bb3204ba0558a3ef1ec4b85fe22
Owen Baldwin <owenabaldwin@gmail.com>
2022-06-01 10:22:48 +0100

    blog now using universal scss in big part

--- src/components/blog-post-3/blog-post-three.component.jsx
+++ src/components/blog-post-3/blog-post-three.component.jsx
@@ -1,4 +1,6 @@
 import './blog-post-three.styles.scss';
+import '../../stylesheets/blog-universal-styles.scss';
+
 import Title from '../title/title.component';
 import BLOG_POST_THREE from '../../json-db/blog-post-3'
 import ForegroundVideo from '../foreground-video/foreground-video.component';
@@ -19,43 +21,35 @@ const BlogPostThree = () => {
         <h2>{text[1].paragraph}</h2>
       </div>
 
-      <div className='blog-post-three-halves'>
-        <div className='blog-post-three-bordered-text'>
-          <p>{text[2].paragraph}</p>
+      <div className='blog-post-vertical-body'>
+        <div className='blog-post-vertical-half'>
+          <div className='blog-post-bordered-text'>
+            <p>{text[2].paragraph}</p>
+          </div>
+          <div className='blog-post-text'>
+            <p>{text[3].paragraph}</p>
+          </div>
+          <img src={photos[3].imageUrl} alt={title} className='blog-post-image-mid'/>
+          <ForegroundVideo videoSrc={videos[0].videoUrl} height="hundredwidth" />
+          <img src={photos[4].imageUrl} alt={title} className='blog-post-image-mid'/>
         </div>
-        <img src={photos[1].imageUrl} alt={title} className='blog-post-three-image-mid'/>
-      </div>
 
-      <div className='blog-post-three-halves'>
-        <div className='blog-post-three-text'>
-          <p>{text[3].paragraph}</p>
-        </div>
-        <img src={photos[2].imageUrl} alt={title} className='blog-post-three-image-mid'/>
-      </div>
-
-      <div className='blog-post-three-halves'>
-        <img src={photos[3].imageUrl} alt={title} className='blog-post-three-image-mid'/>
-        <div className='blog-post-three-bordered-text'>
-          <p>{text[4].paragraph}</p>
-          <p>{text[5].paragraph}</p>
-        </div>
-      </div>
-
-      <div className='blog-post-three-halves'>
-        <ForegroundVideo videoSrc={videos[0].videoUrl} />
-        <div className='blog-post-three-text'>
-          <p>{text[6].paragraph}</p>
-        </div>
-      </div>
-
-      <div className='blog-post-three-halves'>
-        <img src={photos[4].imageUrl} alt={title} className='blog-post-three-image-mid'/>
-        <div className='blog-post-three-bordered-text'>
-          <p>{text[7].paragraph}</p>
-          <a href={link} target='_blank'>Discover more</a>
+        <div className='blog-post-vertical-half'>
+          <img src={photos[1].imageUrl} alt={title} className='blog-post-image-mid'/>
+          <img src={photos[2].imageUrl} alt={title} className='blog-post-image-mid'/>
+          <div className='blog-post-bordered-text'>
+            <p>{text[4].paragraph}</p>
+            <p>{text[5].paragraph}</p>
+          </div>
+          <div className='blog-post-text'>
+            <p>{text[6].paragraph}</p>
+          </div>
+          <div className='blog-post-bordered-text'>
+            <p>{text[7].paragraph}</p>
+            <a href={link} target='_blank'>Discover more</a>
+          </div>
         </div>
       </div>
-
 
       <div className='blog-post-three-video-container'>
         <video loop autoPlay muted className='blog-post-three-video'>
