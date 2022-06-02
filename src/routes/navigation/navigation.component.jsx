18f4b37d9422fd40df67b674c650cdb97d22f0ad
Owen Baldwin <owenabaldwin@gmail.com>
2022-06-02 10:06:59 +0100

    nav btn reduced to three and working apart from contact

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../../components/footer/footer.component';
 import './navigation.styles.scss';
+import { useParams } from 'react-router-dom';
 
 const Navigation = () => {
+  const {urlParam} = useParams();
+
+  console.log("hello");
+  console.log({urlParam});
+  console.log("hello");
+
   return (
     <div className='navigation-container'>
       <div className='navigation-inner'>
@@ -11,12 +18,11 @@ const Navigation = () => {
           <input type="checkbox" class="menu-btn" id="menu-btn" />
           <label for="menu-btn" class="menu-icon"><span class="nav-icon"></span></label>
           <ul class="tabs">
-            <li><p>About</p></li>
-            <li><p>What We Do</p></li>
-            <li><p>How we do it</p></li>
-            <li><p>Case studies</p></li>
-            <li><p>Contact</p></li>
-            <li><p>Blog</p></li>
+            <li><a href="/">Home</a></li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li><a href="/blog">Blog</a></li>
           </ul>
         </div>
       </div>
@@ -27,3 +33,11 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+
+// { {urlParam} === "/blog" ? <a href="#ctc">KontaKt</a> : <a href="/">Contaact</a> }
+
+// <li><p>About</p></li>
+// <li><p>What We Do</p></li>
+// <li><p>How we do it</p></li>
+// <li><p>Case studies</p></li>
