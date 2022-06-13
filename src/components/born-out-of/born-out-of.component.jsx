0c6cff78932ab1d96b8d6a74ba5a0132be0c20f4
Owen Baldwin <owenabaldwin@gmail.com>
2022-06-13 16:24:36 +0100

    gallery now has text and link and brand box comp created

--- src/components/born-out-of/born-out-of.component.jsx
+++ src/components/born-out-of/born-out-of.component.jsx
@@ -1,6 +1,7 @@
 import './born-out-of.styles.scss';
 import BORN_OUT_OF_TEXT from '../../json-db/born-out-of';
 import Title from '../title/title.component';
+import BrandBox from '../brand-box/brand-box.component';
 
 const BornOutOf = () => {
   return (
@@ -8,10 +9,12 @@ const BornOutOf = () => {
       <div className='born-out-of-inner'>
         <Title title="Born out of..." colour="blue" side="right" />
 
-        <div className='born-out-of-text'>
+        <div className='brand-box-container'>
           {BORN_OUT_OF_TEXT.map((object) => {
             return <p key={object.id}>{object.text}</p>
           }) }
+          <div className='brand-box-white-bit'></div>
+
         </div>
       </div>
     </div>
